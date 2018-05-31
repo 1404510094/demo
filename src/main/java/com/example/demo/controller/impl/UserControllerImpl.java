1b431bf007b3c01ee40099c310491fa1b58720e1
cbg <1404510094@qq.com>
2018-05-31 11:21:31 +0800

    增加事物

--- src/main/java/com/example/demo/controller/impl/UserControllerImpl.java
+++ src/main/java/com/example/demo/controller/impl/UserControllerImpl.java
@@ -4,6 +4,7 @@ import com.example.demo.controller.UserController;
 import com.example.demo.domain.entity.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/user")
 public class UserControllerImpl implements UserController{
+
+
 
     @Autowired
     private UserService userService;
 
     @Override
+    @Transactional
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
     public String deleteByPrimaryKey(Integer id) {
+        userService.deleteByPrimaryKey(id);
         return "";
     }
 
     @Override
-    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    @Transactional
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     public String insert(User record) {
         boolean success = userService.insert(record);
         return success == true ? "成功" : "失败";
@@ -30,16 +38,20 @@ public class UserControllerImpl implements UserController{
 
     @Override
     public String selectByPrimaryKey(Integer id) {
+        userService.selectByPrimaryKey(id);
         return null;
     }
 
     @Override
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     public List<User> selectAll() {
-        return null;
+        return userService.selectAll();
     }
 
     @Override
+    @Transactional
     public String updateByPrimaryKey(User record) {
+        userService.updateByPrimaryKey(record);
         return "";
     }
 }
